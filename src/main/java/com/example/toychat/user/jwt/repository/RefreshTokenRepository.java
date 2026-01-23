38e789f876fc1e778e8547ad8e6c2f2cdf713f27
greenPig <1004skfro7@naver.com>
2026-01-23 11:06:16 +0900

    프로젝트 초기 세팅

--- src/main/java/com/example/toychat/user/jwt/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.toychat.user.jwt.repository;
-
-import com.example.toychat.user.jwt.Entity.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken,String> {
-}
